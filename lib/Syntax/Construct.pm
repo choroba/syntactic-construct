e5760acbf85380e82403f157e631317fac61e7a7
E. Choroba <choroba@matfyz.cz>
2015-04-29 09:20:35 +0200

    version bump & Changes

--- lib/Syntax/Construct.pm
+++ lib/Syntax/Construct.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 
 my %construct = (
@@ -86,7 +86,7 @@ Syntax::Construct - Identify which non-feature constructs are used in the code.
 
 =head1 VERSION
 
-Version 0.14
+Version 0.15
 
 =head1 SYNOPSIS
 
