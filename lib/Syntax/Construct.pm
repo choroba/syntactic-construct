ced88e4c3e54a4a76e526aad5b1a1cd4130d8625
E. Choroba <choroba@matfyz.cz>
2016-07-15 17:29:47 +0200

    version bump

--- lib/Syntax/Construct.pm
+++ lib/Syntax/Construct.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 my %introduces = ( 5.024 => [qw[
                                  unicode8.0 \b{lb} sprintf-reorder
@@ -119,7 +119,7 @@ Syntax::Construct - Identify which non-feature constructs are used in the code.
 
 =head1 VERSION
 
-Version 0.26
+Version 0.27
 
 =head1 SYNOPSIS
 
